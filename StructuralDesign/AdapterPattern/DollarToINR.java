dbf6f7b279749071240ee49a0571290e2cc42fe8
sheetalmpatel <shitalpatel9292@gmail.com>
2025-07-22 22:22:27 +0530

    system design pattern codes

---
+++ StructuralDesign/AdapterPattern/DollarToINR.java
@@ -0,0 +1,34 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class DollarToINR {
+    public static void main(String[] args) {
+        System.out.println("Try programiz.pro");
+        Dollar dollar = new Dollar();
+        INRpriceAdapter ia= new INRpriceAdapter(dollar);
+        System.out.println(ia.getINRprice());
+    }
+}
+class Dollar{
+    double getPrice(){
+        return 2;
+    }
+}
+
+interface INRprice{
+    double getINRprice();
+} 
+class INRpriceAdapter implements INRprice{
+    Dollar dollar;
+    INRpriceAdapter(Dollar dollar){
+        this.dollar=dollar;
+    }
+    @Override 
+    public double getINRprice(){
+        return dollar.getPrice() * 80;
+    }
+}
+
+
+
+
