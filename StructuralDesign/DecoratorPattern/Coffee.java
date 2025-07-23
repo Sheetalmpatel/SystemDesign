2b0e7a4e6a3c7ee077a88b3067ef1c8cd8b107d9
sheetalmpatel <shitalpatel9292@gmail.com>
2025-07-23 22:33:42 +0530

    coffee.java

--- StructuralDesign/DecoratorPattern/Coffee.java
+++ StructuralDesign/DecoratorPattern/Coffee.java
@@ -2,74 +2,86 @@
 // Author : sheetal
 public class Coffee {
     public static void main(String[] args) {
-        BasicCoffee coffee= new simpleCoffee();
+        BasicCoffee coffee = new simpleCoffee();
         System.out.println("Description: " + coffee.getDescription());
         System.out.println("Price: " + coffee.price());
 
         coffee = new MilkDecorator(coffee);
         System.out.println("Description: " + coffee.getDescription());
-                System.out.println("Price: " + coffee.price());
-
+        System.out.println("Price: " + coffee.price());
 
         coffee = new SugarDecorator(coffee);
         System.out.println("Description: " + coffee.getDescription());
-                System.out.println("Price: " + coffee.price());
+        System.out.println("Price: " + coffee.price());
 
     }
 }
-interface  BasicCoffee{
+
+interface BasicCoffee {
     String getDescription();
+
     double price();
 }
 
-class simpleCoffee implements BasicCoffee{
+class simpleCoffee implements BasicCoffee {
     @Override
     public String getDescription() {
         return "Simple Coffee";
     }
+
     @Override
-    public double price(){
+    public double price() {
         return 100;
     }
 }
-abstract  class coffeeDecorator implements  BasicCoffee{
+
+abstract class coffeeDecorator implements BasicCoffee {
     BasicCoffee basicCoffee;
-    public coffeeDecorator(BasicCoffee basicCoffee){
-        this.basicCoffee=basicCoffee;
+
+    public coffeeDecorator(BasicCoffee basicCoffee) {
+        this.basicCoffee = basicCoffee;
     }
 
     @Override
-    public String getDescription(){
+    public String getDescription() {
         return basicCoffee.getDescription();
     }
+
     @Override
-    public double price(){
+    public double price() {
         return basicCoffee.price();
     }
 
 }
 
-class MilkDecorator extends coffeeDecorator{
-    public MilkDecorator(BasicCoffee basicCoffee){
+class MilkDecorator extends coffeeDecorator {
+    public MilkDecorator(BasicCoffee basicCoffee) {
         super(basicCoffee);
     }
 
-    public String getDescription(){
-        return super.getDescription()+"milk";
+    @Override
+    public String getDescription() {
+        return super.getDescription() + "milk";
     }
-    public double price(){
-        return super.price()+20;
+
+    @Override
+    public double price() {
+        return super.price() + 20;
     }
 }
-class SugarDecorator extends coffeeDecorator{
-    public SugarDecorator(BasicCoffee basicCoffee){
+
+class SugarDecorator extends coffeeDecorator {
+    public SugarDecorator(BasicCoffee basicCoffee) {
         super(basicCoffee);
     }
 
-    public String getDescription(){
-        return super.getDescription()+"sugar";
+    @Override
+    public String getDescription() {
+        return super.getDescription() + "sugar";
     }
-    public double price(){
-        return super.price()+10;
+
+    @Override
+    public double price() {
+        return super.price() + 10;
     }
 }
